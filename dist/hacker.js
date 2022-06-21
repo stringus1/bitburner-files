bff345402399e910708a5940802577eefcce568d
Stringus1 <str.bean.joe+github.private@gmail.com>
2022-06-21 02:24:23 -0700

    a

--- dist/hacker.js
+++ dist/hacker.js
@@ -1,12 +1,16 @@
 /** @param {NS} ns */
 export async function main(ns) {
     const host = ns.getHostname();
+    if (host === "home") {
+        return;
+    }
     for (;;) {
         while (ns.hackAnalyzeChance(host) < 0.4) {
             await ns.weaken(host);
         }
-        while (ns.getServerMoneyAvailable(host) / ns.getServerMaxMoney(host) <
-            0.1) {
+        for (let i = 0; i < 3; i++) {
+            if (ns.getServerMoneyAvailable(host) / ns.getServerMaxMoney(host) > 0.1)
+                break;
             await ns.grow(host);
         }
         await ns.hack(host);
--- dist/traverse.js
+++ dist/traverse.js
@@ -1,52 +1,52 @@
 async function getServerList(ns) {
-    return (await ns.scan()).map((hostname) => ({
-        hostname,
-        // requiredHackingSkill: ns.getServerRequiredHackingLevel(hostname),
-        // hasAdminRights: ns.hasRootAccess(hostname),
-    }));
-    // .filter(server => server.hasAdminRights)
+  return (await ns.scan()).map((hostname) => ({
+    hostname,
+    // requiredHackingSkill: ns.getServerRequiredHackingLevel(hostname),
+    // hasAdminRights: ns.hasRootAccess(hostname),
+  }));
+  // .filter(server => server.hasAdminRights)
 }
 const TRAVERSE_NAME = "traverse.js";
-const TRAVERSE_URL = "https://raw.githubusercontent.com/stringus1/bitburner-files/master/dist/traverse.js";
+const TRAVERSE_URL =
+  "https://raw.githubusercontent.com/stringus1/bitburner-files/master/dist/traverse.js";
 const TRAVERSE_RAM = 5.5;
 const PAYLOAD_NAME = "hacker.js";
-const PAYLOAD_URL = "https://raw.githubusercontent.com/stringus1/bitburner-files/master/dist/hacker.js";
+const PAYLOAD_URL =
+  "https://raw.githubusercontent.com/stringus1/bitburner-files/master/dist/hacker.js";
 const PAYLOAD_RAM = 3.25;
 /** @param {NS} ns */
 export async function main(ns) {
-    const currentHost = ns.getHostname();
-    let blacklist = ns.args.map((elem) => elem.toString());
-    if (!blacklist.includes("home")) {
-        blacklist = ["home", ...blacklist];
-    }
-    const serverList = ns.scan().filter((host) => !blacklist.includes(host));
-    ns.tprint(`${currentHost}:${ns.getScriptName()}: ${serverList}`);
-    for (let hostname of serverList) {
-        await ns.wget(TRAVERSE_URL, TRAVERSE_NAME, hostname);
-        await ns.wget(PAYLOAD_URL, PAYLOAD_NAME, hostname);
-        // await ns.scp(ns.getScriptName(), hostname)
-        // await ns.scp(PAYLOAD_NAME, hostname)
-        ns.exec(TRAVERSE_NAME, hostname, 1, hostname, ...blacklist);
-    }
-    for (let port of [
-        ns.ftpcrack,
-        ns.relaysmtp,
-        ns.brutessh,
-        ns.httpworm,
-        ns.ftpcrack,
-    ]) {
-        try {
-            port(currentHost);
-        }
-        catch { }
-    }
+  const currentHost = ns.getHostname();
+  let blacklist = ns.args.map((elem) => elem.toString());
+  if (!blacklist.includes("home")) {
+    blacklist = ["home", ...blacklist];
+  }
+  const serverList = ns.scan().filter((host) => !blacklist.includes(host));
+  ns.tprint(`${currentHost}:${ns.getScriptName()}: ${serverList}`);
+  for (let hostname of serverList) {
+    await ns.wget(TRAVERSE_URL, TRAVERSE_NAME, hostname);
+    await ns.wget(PAYLOAD_URL, PAYLOAD_NAME, hostname);
+    // await ns.scp(ns.getScriptName(), hostname)
+    // await ns.scp(PAYLOAD_NAME, hostname)
+    ns.exec(TRAVERSE_NAME, hostname, 1, hostname, ...blacklist);
+  }
+  for (let port of [
+    ns.ftpcrack,
+    ns.relaysmtp,
+    ns.brutessh,
+    ns.httpworm,
+    ns.ftpcrack,
+  ]) {
     try {
-        ns.nuke(currentHost);
-    }
-    catch { }
-    const [total, used] = ns.getServerRam(currentHost);
-    const free = total - used + TRAVERSE_RAM;
-    const threads = Math.floor(free / PAYLOAD_RAM);
-    ns.spawn(PAYLOAD_NAME, threads, ...blacklist);
-    //ns.exec(SCRIPT_NAME, currentHost, 1, ...blacklist)
+      port(currentHost);
+    } catch {}
+  }
+  try {
+    ns.nuke(currentHost);
+  } catch {}
+  const [total, used] = ns.getServerRam(currentHost);
+  const free = total - used + TRAVERSE_RAM;
+  const threads = Math.floor(free / PAYLOAD_RAM);
+  ns.spawn(PAYLOAD_NAME, threads, ...blacklist);
+  //ns.exec(SCRIPT_NAME, currentHost, 1, ...blacklist)
 }
